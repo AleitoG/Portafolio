b710330b8873282f96bde5e9d461fc4ee80b4c8d
Alejandro Torres <aleth2k@gmail.com>
2024-03-20 00:19:49 -0600

    porfin

--- docs/index.js
+++ docs/index.js
@@ -31,4 +31,22 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  document.getElementById('CV').addEventListener('click', function() {
+    var pdfUrl = 'Assets/CV.pdf';
+
+    // Crear un enlace temporal
+    var a = document.createElement('a');
+    a.href = pdfUrl;
+
+    // Establecer el atributo download con el nombre del archivo
+    a.download = 'archivo.pdf';
+
+    // Simular un clic en el enlace para iniciar la descarga
+    document.body.appendChild(a);
+    a.click();
+
+    // Eliminar el enlace temporal
+    document.body.removeChild(a);
+});
   
